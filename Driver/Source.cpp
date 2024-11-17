09ad4b1f17cbcebb1c7369285d8d3b3b12b2e561
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-11-17 22:40:33 +0200

    added code from course

--- Driver/Source.cpp
+++ Driver/Source.cpp
@@ -1,5 +1,6 @@
 #include <ndis.h>
 #include <ntddk.h>
+#include <stdio.h> // sprintf()
 
 // These cannot be const nor constexpr!
 UNICODE_STRING DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\IDPS Sniffer Device");
@@ -10,9 +11,35 @@ UNICODE_STRING SYMLINK_NAME = RTL_CONSTANT_STRING(L"\\??\\SnifferDeviceLink");
 
 // Function declarations
 extern "C" DRIVER_INITIALIZE DriverEntry;
-DRIVER_UNLOAD DriverUnload;
-NTSTATUS DriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-NTSTATUS DriverDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+extern "C" DRIVER_UNLOAD DriverUnload;
+//extern "C" NTSTATUS DriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+extern "C" NTSTATUS DriverDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+
+extern "C" NTSTATUS DriverPassThru([[maybe_unused]] PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
+    NTSTATUS status = STATUS_SUCCESS;
+
+    switch (irpSp->MajorFunction)
+    {
+    case IRP_MJ_CREATE:
+        KdPrint(("Create request!\n"));
+        break;
+    case IRP_MJ_CLOSE:
+        KdPrint(("Close request!\n"));
+        break;
+    case IRP_MJ_READ:
+        KdPrint(("Read request!\n"));
+        break;
+    default:
+        break;
+    }
+
+    Irp->IoStatus.Information = 0;
+    Irp->IoStatus.Status = status;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    return status;
+}
 
 // Entry point
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, [[maybe_unused]] PUNICODE_STRING RegistryPath)
@@ -22,11 +49,21 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, [[maybe_unused]] PU
 
     PDEVICE_OBJECT deviceObject;
 
-    // Create the device                                                        TODO: IS TYPE CORRECT??
-    status = IoCreateDevice(DriverObject, sizeof(NDIS_HANDLE), &DEVICE_NAME, FILE_DEVICE_NETWORK, FILE_DEVICE_SECURE_OPEN, FALSE, &deviceObject);
+    // Create the device                                                   TODO: IS TYPE CORRECT??
+    status = IoCreateDevice(DriverObject, 0, &DEVICE_NAME, FILE_DEVICE_NETWORK, FILE_DEVICE_SECURE_OPEN, FALSE, &deviceObject);
     if (!NT_SUCCESS(status))
     {
-        KdPrint(("FAILED to create device!\n"));
+        // Print error with status code
+        if (status == STATUS_OBJECT_NAME_COLLISION)
+            KdPrint(("FAILED to create device: NAME_COLLISION\n"));
+        else
+        {
+            KdPrint(("FAILED to create device:\n"));
+            char num_str[sizeof(LONG) + 2] = {0};
+            sprintf(num_str, "%ld\n", status);
+            KdPrint((num_str));
+        }
+
         return status;
     }
 
@@ -45,13 +82,16 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, [[maybe_unused]] PU
     //DriverObject->MajorFunction[IRP_MJ_CREATE] = DriverCreateClose;
     //DriverObject->MajorFunction[IRP_MJ_CLOSE] = DriverCreateClose;
     //DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DriverDeviceControl;
+    for (int i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; ++i)
+        DriverObject->MajorFunction[i] = DriverPassThru;
+
     DriverObject->DriverUnload = DriverUnload;
 
     return STATUS_SUCCESS;
 }
 
 // Unload routine
-VOID DriverUnload(PDRIVER_OBJECT DriverObject)
+extern "C" void DriverUnload(PDRIVER_OBJECT DriverObject)
 {
     IoDeleteSymbolicLink(&SYMLINK_NAME);
     IoDeleteDevice(DriverObject->DeviceObject);
@@ -68,7 +108,7 @@ VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 //}
 
 // Device control routine
-NTSTATUS DriverDeviceControl([[maybe_unused]] PDEVICE_OBJECT DeviceObject, PIRP Irp)
+extern "C" NTSTATUS DriverDeviceControl([[maybe_unused]] PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
     PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
     NTSTATUS status;
