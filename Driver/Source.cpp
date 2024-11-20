05d91877dc6ccc482f54fb114f09f9906d17a449
Gil_Yelkin <gil.yelkin@gmail.com>
2024-11-20 23:00:59 +0200

    fml

--- Driver/Source.cpp
+++ Driver/Source.cpp
@@ -1,29 +1,49 @@
-#include <ndis.h>
-#include <ntddk.h>
-#include <stdio.h> // sprintf()
-#include <fwpmk.h>
+#define INITGUID
+#include <guiddef.h> 
 #include <fwpsk.h>
+#include <fwpmk.h>
 #include <fwpmu.h>
+//#include <Windows.h>
+//#include <ntddk.h>
+//#include <ndis.h>
+//#include <ntdef.h>
+//#include <stdio.h> // sprintf()
+//#include <wchar.h>
+
 
 // These cannot be const nor constexpr!
 UNICODE_STRING DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\IDPS Sniffer Device");
 UNICODE_STRING SYMLINK_NAME = RTL_CONSTANT_STRING(L"\\??\\SnifferDeviceLink");
-UINT32 REG_CALLOUT_ID = 0;
-UINT32 ADD_CALLOUT_ID = 1;
 
 // Global variables
+#define __IGNORE [[maybe_unused]]
 PDEVICE_OBJECT deviceObject = NULL;
 HANDLE engineHandle = NULL;
+UINT32 RegCalloutId = 0;
+UINT32 AddCalloutId = 0;
+UINT64 FilterId = 0;
 
 // IOCTL code for user-mode communication
 #define IOCTL_READ_RAW_PACKET CTL_CODE(FILE_DEVICE_NETWORK, 0x801, METHOD_BUFFERED, FILE_READ_DATA | FILE_WRITE_DATA)
 DEFINE_GUID(WFP_SAMPLE_ESTABLISHED_CALLOUT_V4_GUID, 0xd969fc67, 0x6fb2, 0x4504, 0x91, 0xce, 0xa9, 0x7c, 0x3c, 0x32, 0xad, 0x36);
+DEFINE_GUID(WFP_SAMPLE_SUB_LAYER_GUID, 0xed6a516a, 0x36d1, 0x4881, 0xbc, 0xf0, 0xac, 0xeb, 0x4c, 0x4, 0xc2, 0x1c);
 
 // Function declarations
-extern "C" DRIVER_UNLOAD DriverUnload;
+DRIVER_UNLOAD DriverUnload;
 //extern "C" NTSTATUS DriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-extern "C" NTSTATUS DriverDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-extern "C" NTSTATUS DriverPassThru(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS DriverDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS DriverPassThru(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS WfpOpenEngine();
+NTSTATUS WfpAddCallout();
+NTSTATUS WfpAddSublayer();
+VOID UnInitWfp();
+NTSTATUS InitializeWfp();
+NTSTATUS WfpAddFilter();
+NTSTATUS WfpRegisterCallout();
+VOID FilterCallback(__IGNORE const FWPS_INCOMING_VALUES0* inFixedValues, __IGNORE const FWPS_INCOMING_METADATA_VALUES0* inMetaValues, PVOID layerData, __IGNORE const PVOID context, const FWPS_FILTER* filter, __IGNORE UINT64 flowContext, FWPS_CLASSIFY_OUT* classifyOut);
+NTSTATUS NotifyCallback(__IGNORE FWPS_CALLOUT_NOTIFY_TYPE type, __IGNORE const GUID* filterKey, __IGNORE FWPS_FILTER* filter);
+VOID FlowDeleteCallback(__IGNORE UINT16 layerId, __IGNORE UINT32 calloutId, __IGNORE UINT64 flowContext);
+
 
 // Entry point
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, [[maybe_unused]] PUNICODE_STRING RegistryPath)
@@ -154,26 +174,102 @@ extern "C" NTSTATUS DriverDeviceControl([[maybe_unused]] PDEVICE_OBJECT DeviceOb
 
 // ######## NETWORK RELATED ########
 
+NTSTATUS WfpOpenEngine()
+{
+    return FwpmEngineOpen(NULL, RPC_C_AUTHN_WINNT, NULL, NULL, &engineHandle);
+}
+
+NTSTATUS WfpAddCallout()
+{
+    wchar_t* displayName = wcsdup(L"EstablishedCalloutName");
+    FWPM_CALLOUT callout = { 0 };
+    callout.flags = 0;
+    callout.displayData.name = displayName;
+    callout.displayData.description = displayName;
+    callout.calloutKey = WFP_SAMPLE_ESTABLISHED_CALLOUT_V4_GUID;
+    callout.applicableLayer = FWPM_LAYER_STREAM_V4;
+
+    return FwpmCalloutAdd(engineHandle, &callout, NULL, &AddCalloutId);
+}
+
+NTSTATUS WfpAddSublayer()
+{
+    wchar_t* displayName = wcsdup(L"EstablishedSublayerName");
+    FWPM_SUBLAYER sublayer = { 0 };
+    sublayer.displayData.name = displayName;
+    sublayer.displayData.description = displayName;
+    sublayer.subLayerKey = WFP_SAMPLE_SUB_LAYER_GUID;
+    sublayer.weight = 65500;
+    return FwpmSubLayerAdd(engineHandle, &sublayer, NULL);
+}
+
+VOID UnInitWfp()
+{
+    if (!engineHandle)
+        return;
+
+    if (FilterId != 0)
+    {
+        FwpmFilterDeleteById(engineHandle, FilterId);
+        FwpmSubLayerDeleteByKey(engineHandle, &WFP_SAMPLE_SUB_LAYER_GUID);
+    }
+
+    if (AddCalloutId != 0)
+    {
+        FwpmCalloutDeleteById(engineHandle, AddCalloutId);
+    }
+
+    if (RegCalloutId != 0)
+    {
+        FwpsCalloutUnregisterById(RegCalloutId);
+    }
+
+    FwpmEngineClose(engineHandle);
+    engineHandle = NULL;
+}
+
 NTSTATUS InitializeWfp()
 {
-    NTSTATUS status = FwpmEngineOpen(NULL, RPC_C_AUTHN_WINNT, NULL, NULL, &engineHandle);
-    if (!NT_SUCCESS(status))
+    if (NT_SUCCESS(FwpmEngineOpen(NULL, RPC_C_AUTHN_WINNT, NULL, NULL, &engineHandle)) &&
+        NT_SUCCESS(WfpRegisterCallout()) &&
+        NT_SUCCESS(WfpAddCallout()) &&
+        NT_SUCCESS(WfpAddSublayer()) &&
+        NT_SUCCESS(WfpAddFilter()))
     {
-        UnInitWfp();
-        return STATUS_UNSUCCESSFUL;
+        return STATUS_SUCCESS;
     }
 
-    if (!NT_SUCCESS(WfpRegisterCallout()))
-    {
-        UnInitWfp();
-        return STATUS_UNSUCCESSFUL;
-    }
+    UnInitWfp();
+    return STATUS_UNSUCCESSFUL;
+}
 
+NTSTATUS WfpAddFilter()
+{
+    wchar_t* displayName = wcsdup(L"EstablishedSublayerName");
+    FWPM_FILTER filter = { 0 };
+    FWPM_FILTER_CONDITION condition[1] = {0};
+
+    filter.displayData.name = displayName;
+    filter.displayData.description = displayName;
+    filter.layerKey = FWPM_LAYER_STREAM_V4;
+    filter.subLayerKey = WFP_SAMPLE_SUB_LAYER_GUID;
+    filter.weight.type = FWP_EMPTY;
+    filter.numFilterConditions = 1;
+    filter.filterCondition = condition;
+    filter.action.type = FWP_ACTION_CALLOUT_TERMINATING;
+    filter.action.calloutKey = WFP_SAMPLE_ESTABLISHED_CALLOUT_V4_GUID;
+
+    condition[0].fieldKey = FWPM_CONDITION_IP_LOCAL_PORT;
+    condition[0].matchType = FWP_MATCH_LESS_OR_EQUAL;
+    condition[0].conditionValue.type = FWP_UINT16;
+    condition[0].conditionValue.uint16 = 65000;
+
+    return FwpmFilterAdd(engineHandle, &filter, NULL, &FilterId);
 }
 
 NTSTATUS WfpRegisterCallout()
 {
-    const FWPS_CALLOUT callout = {
+    FWPS_CALLOUT callout = {
         .calloutKey = WFP_SAMPLE_ESTABLISHED_CALLOUT_V4_GUID,
         .flags = 0,
         .classifyFn = FilterCallback,
@@ -181,33 +277,35 @@ NTSTATUS WfpRegisterCallout()
         .flowDeleteFn = FlowDeleteCallback
     };
 
-    return FwpsCalloutRegister(deviceObject, &callout, &REG_CALLOUT_ID);
+    return FwpsCalloutRegister(deviceObject, &callout, &RegCalloutId);
 }
 
-void FilterCallback(const FWPS_INCOMING_VALUES* inFixedValues, const FWPS_INCOMING_METADATA_VALUES0* inMetaValues, void* layerData, const void* context, const FWPS_FILTER* filter, UINT64 flowContext, FWPS_CLASSIFY_OUT* classifyOut)
+VOID FilterCallback(__IGNORE const FWPS_INCOMING_VALUES0* inFixedValues, __IGNORE const FWPS_INCOMING_METADATA_VALUES0* inMetaValues, PVOID layerData, __IGNORE const PVOID context, const FWPS_FILTER* filter, __IGNORE UINT64 flowContext, FWPS_CLASSIFY_OUT* classifyOut)
 {
+    FWPS_STREAM_CALLOUT_IO_PACKET* packet;
+    KdPrint(("data is here\r\n"));
 
+    packet = (FWPS_STREAM_CALLOUT_IO_PACKET*)layerData;
+
+    RtlZeroMemory(classifyOut, sizeof(FWPS_CLASSIFY_OUT));
+
+    packet->streamAction = FWPS_STREAM_ACTION_NONE;
+    classifyOut->actionType = FWP_ACTION_PERMIT;
+    if (filter->flags && FWPS_FILTER_FLAG_CLEAR_ACTION_RIGHT)
+    {
+        classifyOut->actionType &= FWPS_RIGHT_ACTION_WRITE;
+    }
 }
 
 // Boilerplate function without any use for now
-NTSTATUS NotifyCallback(FWPS_CALLOUT_NOTIFY_TYPE type, const GUID* filterKey, FWPS_FILTER* filter)
+NTSTATUS NotifyCallback(__IGNORE FWPS_CALLOUT_NOTIFY_TYPE type, __IGNORE const GUID* filterKey, __IGNORE FWPS_FILTER* filter)
 {
     return STATUS_SUCCESS;
 }
 
 // Boilerplate function without any use for now
-VOID FlowDeleteCallback(UINT16 layerId, UINT32 calloutId, UINT64 flowContext)
+VOID FlowDeleteCallback(__IGNORE UINT16 layerId, __IGNORE UINT32 calloutId, __IGNORE UINT64 flowContext)
 {
     // Noting here!
 }
 
-NTSTATUS WfpAddCallout()
-{
-    wchar_t* displayName = wcsdup(L"EstablishedCalloutName");
-    FWPM_CALLOUT callout = {
-        .flags = 0,
-    };
-    callout.displayData.name = displayName;
-
-    FwpmCalloutAdd(engineHandle, &callout, NULL, &ADD_CALLOUT_ID);
-}
