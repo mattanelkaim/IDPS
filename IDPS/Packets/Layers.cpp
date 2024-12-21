b0e4d11f624647ac3678774aabe62e42961d926c
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-12-22 00:39:50 +0200

    Created a simple draft of ethernet constructor

---
+++ IDPS/Packets/Layers.cpp
@@ -0,0 +1,9 @@
+//#include "Layers.h"
+//#include <cstring> // std::memcpy
+
+//EthernetHeader::EthernetHeader(const uint8_t* rawData)
+//{
+//    std::memcpy(this->destinationMAC, rawData, 6);
+//    std::memcpy(this->sourceMAC, rawData + 6, 6);
+//    //this->etherType = (rawData[12] << 8) | rawData[13];
+//}
