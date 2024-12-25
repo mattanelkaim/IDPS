7aef441780dd8e09e981037b69039364586c3163
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-12-25 17:13:33 +0200

    ipToString now works

--- IDPS/Helper.h
+++ IDPS/Helper.h
@@ -1,6 +1,5 @@
 #pragma once
 
-//#include "Packets/Layers.h"
 #include <bit>
 #include <concepts>
 #include <cstdint>
@@ -19,39 +18,40 @@ enum ProtocolCode_8 : uint8_t
     UDP = 0x11,
 };
 
-//namespace Helper
-//{
-//    std::string ipToString(const uint32_t ip) noexcept
-//    {
-//        // Allocate a buffer large enough for "255.255.255.255\0" (16 bytes max)
-//        char buffer[16];
-//
-//        // Extract and format directly into the buffer
-//        std::snprintf(buffer, sizeof(buffer), "%u.%u.%u.%u",
-//                      (ip >> 24) & 0xFF,
-//                      (ip >> 16) & 0xFF,
-//                      (ip >> 8) & 0xFF,
-//                      ip & 0xFF);
-//
-//        // Return the formatted string (this is optimal)
-//        return std::string(buffer);
-//    }
-//}
-
 
 template <typename T>
 concept IntegralOrProtocolCode_16 = std::integral<T> || std::is_same_v<T, ProtocolCode_16>;
 
-template <IntegralOrProtocolCode_16 T>
-constexpr T toBigEndian(const T& val) noexcept
+// For some reason, all functions MUST BE INLINE
+namespace Helper
 {
-    if constexpr (std::endian::native == std::endian::big)
-        return val;
-    else // Swap bytes to Big Endian
+    inline std::string ipToString(const uint32_t ip)
     {
-        if constexpr (std::is_same_v<T, ProtocolCode_16>)
-            return static_cast<T>(std::byteswap(static_cast<uint16_t>(val)));
-        else
-            return std::byteswap(val);
+        // Allocate a buffer large enough for "255.255.255.255\0" (16 bytes max)
+        char buffer[16];
+
+        // Extract and format directly into the buffer
+        std::snprintf(buffer, sizeof(buffer), "%u.%u.%u.%u",
+                      (ip >> 24) & 0xFF,
+                      (ip >> 16) & 0xFF,
+                      (ip >> 8) & 0xFF,
+                      ip & 0xFF);
+
+        // Return the formatted string (this is optimal)
+        return std::string(buffer);
+    }
+
+    template <IntegralOrProtocolCode_16 T>
+    constexpr T toBigEndian(const T& val) noexcept // constexpr is inherently inline
+    {
+        if constexpr (std::endian::native == std::endian::big)
+            return val;
+        else // Swap bytes to Big Endian
+        {
+            if constexpr (std::is_same_v<T, ProtocolCode_16>)
+                return static_cast<T>(std::byteswap(static_cast<uint16_t>(val)));
+            else
+                return std::byteswap(val);
+        }
     }
 }
