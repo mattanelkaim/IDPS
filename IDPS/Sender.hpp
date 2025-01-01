9c814a2eefac194900f62f559f434805027717de
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2025-01-01 12:39:05 +0200

    optimized code

--- IDPS/Sender.hpp
+++ IDPS/Sender.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+// Do NOT sort these includes
 #include <winsock2.h>
-#include <ws2tcpip.h>
 #include <iphlpapi.h>
-#include <stdio.h>
+#include <ws2tcpip.h>
 
+// Link with Ws2_32.lib and Iphlpapi.lib (for GetAdaptersInfo)
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Iphlpapi.lib")
 
@@ -12,11 +13,11 @@ namespace Sender
 {
     bool GetBroadcastAddress(const char* interfaceName, in_addr& broadcast)
     {
-        ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO); // Cannot be const
+        ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO); // Cannot be const because GetAdaptersInfo changes it
         PIP_ADAPTER_INFO AdapterInfo = (IP_ADAPTER_INFO*)malloc(ulOutBufLen);
-        if (AdapterInfo == NULL)
+        if (AdapterInfo == NULL) [[unlikely]]
         {
-            printf("Error allocating memory needed to call GetAdaptersInfo\n");
+            perror("Error allocating memory needed to call GetAdaptersInfo");
             return false;
         }
 
@@ -26,32 +27,37 @@ namespace Sender
             // Re-allocate the memory and try again (ulOutBufLen now has the correct size)
             free(AdapterInfo);
             AdapterInfo = (IP_ADAPTER_INFO*)malloc(ulOutBufLen);
-            if (AdapterInfo == NULL)
+            if (AdapterInfo == NULL) [[unlikely]]
             {
-                printf("Error allocating memory needed to call GetAdaptersInfo\n");
+                perror("Error allocating memory needed to call GetAdaptersInfo");
                 return false;
             }
         }
         
         // Call function again with the correct buffer size
-        if (GetAdaptersInfo(AdapterInfo, &ulOutBufLen) == NO_ERROR)
+        if (GetAdaptersInfo(AdapterInfo, &ulOutBufLen) == NO_ERROR) [[likely]]
         {
+            // Iterate over the list of adapters until target adapter is found
             PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo; // Temp pointer to iterate over the list
             while (pAdapterInfo)
             {
-                printf("%s: %s\n", pAdapterInfo->Description, pAdapterInfo->IpAddressList.IpAddress.String);
+                printf("%s: %s\n", pAdapterInfo->Description, pAdapterInfo->IpAddressList.IpAddress.String); // TEMP FOR DEBUGGING
                 if (strcmp(pAdapterInfo->Description, interfaceName) == 0)
                 {
+                    // Convert string to in_addr and change last byte to 255
                     inet_pton(AF_INET, pAdapterInfo->IpAddressList.IpAddress.String, &broadcast);
                     broadcast.s_impno = 255;
+                    free(AdapterInfo);
                     return true;
                 }
 
                 pAdapterInfo = pAdapterInfo->Next; // Else move to the next adapter
             }
         }
-        else
-            printf("Call to GetAdaptersInfo failed.\n");
+        else [[unlikely]]
+        {
+            perror("Call to GetAdaptersInfo failed.");
+        }
 
         free(AdapterInfo);
         return false;
