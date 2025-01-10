79dfac35bd2a66c1889fac6f4729507aa656a248
Gil_Yelkin <gil.yelkin@gmail.com>
2025-01-10 15:01:29 +0200

    implemented extractor thread routine

--- IDPS/PacketExtractor.cpp
+++ IDPS/PacketExtractor.cpp
@@ -1,13 +1,83 @@
 #include "PacketExtractor.h"
+#include <fstream>
 
-PacketExtractor::PacketExtractor() noexcept : packetQueue(), queueMutex()
+PacketExtractor::PacketExtractor() noexcept : packetQueue(), queueMutex(), extractorThread(&PacketExtractor::threadRoutine, this)
 {
-	this->ExtractorThread = std::thread(&PacketExtractor::threadRoutine, this);
-	this->ExtractorThread.detach(); // thread works in the background
+	this->extractorThread.detach(); // letting packet extractor thread work in the background
+
+	// obtaining a device handle to the driver
+	this->deviceHandle = CreateFileW(L"\\\\.\\SnifferDeviceLink", GENERIC_ALL, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, NULL);
+	if (this->deviceHandle == INVALID_HANDLE_VALUE)
+		throw std::runtime_error("Please run the driver prior to running the IDPS.");
 }
 
-void PacketExtractor::threadRoutine() noexcept
+void PacketExtractor::threadRoutine()
 {
+	uint16_t packetSize = 0;
+	std::vector<char> rawPacket;
+	bool pending = false;
+	std::ifstream packetFile(PACKET_FILE_PATH, std::ios::binary);
+
+	if (!packetFile.is_open())
+		throw std::runtime_error("Error opening packet file.");
+
+    while (true)
+    {
+		if (packetFile.eof())
+		{
+			packetFile.clear();
+
+			// truncate the file every time the thread reaches its end
+			if (packetFile.tellg() != std::ios::beg)
+				truncatePacketFile();
+
+			packetFile.seekg(0, std::ios::beg);
+			// locking the queue until new packets arrive
+			if (!pending)
+			{
+				this->queueMutex.lock();
+				pending = true;
+			}
+			continue;
+		}
+
+		// reading packet size and data
+		while (!this->bytesAvailable(packetFile, sizeof(packetSize))); { (void)0; }
+		packetFile.read(reinterpret_cast<char*>(&packetSize), sizeof(packetSize));
+		rawPacket.resize(packetSize);
+		while (!this->bytesAvailable(packetFile, packetSize)); { (void)0; }
+		packetFile.read(rawPacket.data(), packetSize);
+
+		if (pending)
+			pending = false;
+		else
+			this->queueMutex.lock();
+
+		this->packetQueue.push(rawPacket);
+		this->queueMutex.unlock();
+	}
+
+	packetFile.close();
+}
+
+void PacketExtractor::truncatePacketFile() const noexcept
+{
+	if (!DeviceIoControl(deviceHandle, IOCTL_TRUNCATE_FILE, NULL, 0, NULL, 0, NULL, NULL));
+		throw std::runtime_error("Please run the driver prior to running the IDPS.");
+}
+
+bool PacketExtractor::bytesAvailable(std::ifstream& file, const uint16_t numBytes) noexcept
+{
+    const std::streampos currentPos = file.tellg();
+    file.seekg(0, std::ios::end);
+	const std::streampos endPos = file.tellg();
+    file.seekg(currentPos, std::ios::beg);
+    return (endPos - currentPos) >= numBytes;
+}
+
+PacketExtractor::~PacketExtractor() noexcept
+{
+	CloseHandle(deviceHandle); // No need to validate before or after closing
 }
 
 PacketExtractor& PacketExtractor::getInstance() noexcept
@@ -16,18 +86,15 @@ PacketExtractor& PacketExtractor::getInstance() noexcept
 	return instance;
 }
 
-std::vector<const uint8_t> PacketExtractor::getPacket()
+std::vector<char> PacketExtractor::getPacket() noexcept
 {
-    std::vector<const uint8_t> toReturn;
+    std::vector<char> toReturn;
 
-    if (this->packetQueue.empty())
-    {
-        
-    }
-
+	// loading new packet
     this->queueMutex.lock();
     toReturn = this->packetQueue.front();
     this->packetQueue.pop();
     this->queueMutex.unlock();
+
     return toReturn;
 }
--- IDPS/PacketExtractor.h
+++ IDPS/PacketExtractor.h
@@ -1,31 +1,35 @@
 #pragma once
 
 #include "../Driver/LayerHandles.h"
-#include "Helper.h"
 #include <thread>
 #include <queue>
 #include <vector>
 #include <mutex>
+#include <atomic>
 
 class PacketExtractor final
 {
 private:
+	std::queue<std::vector<char>> packetQueue;
+	std::thread extractorThread;
 	std::mutex queueMutex;
-	std::queue<std::vector<const uint8_t>> packetQueue;
-	std::thread ExtractorThread;
+	HANDLE deviceHandle;
 
 	// private methods
 	PacketExtractor() noexcept;
-	void threadRoutine() noexcept;
+	void threadRoutine();
+	void truncatePacketFile() const noexcept;
+	static bool bytesAvailable(std::ifstream& file, uint16_t numBytes) noexcept;
 
 public:
-	// singelton functions
+
+	// singelton methods
 	PacketExtractor(const PacketExtractor& other) = delete;
 	void operator=(const PacketExtractor& other) = delete;
-	~PacketExtractor() noexcept = default;
+	~PacketExtractor() noexcept;
 
 	// methods
 	static PacketExtractor& getInstance() noexcept;
-	std::vector<const uint8_t> getPacket() noexcept;
+	std::vector<char> getPacket() noexcept;
 };
 
