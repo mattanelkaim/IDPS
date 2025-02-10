819117eac54f90b90745f47596f2be340d126b5f
Gil_Yelkin <gil.yelkin@gmail.com>
2025-02-10 19:33:03 +0200

    refactored PacketExtractor to use the new DriverCommunicator

--- IDPS/PacketExtractor.h
+++ IDPS/PacketExtractor.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../Driver/LayerHandles.h"
 #include <cstdint>
 #include <mutex>
 #include <queue>
@@ -13,17 +12,15 @@ private:
 	std::mutex m_queueMutex;
 	std::queue<std::vector<char>> m_packetQueue;
 	std::thread m_extractorThread;
-	HANDLE m_deviceHandle;
 
 	// private methods
 	PacketExtractor();
 	void threadRoutine();
-	void truncatePacketFile() const;
 	static bool areBytesAvailable(std::ifstream& file, uint16_t numBytes) noexcept;
 
 public:
 	// singelton methods
-	~PacketExtractor() noexcept;
+	~PacketExtractor() noexcept = default;
 	static PacketExtractor& getInstance() noexcept;
 	PacketExtractor(const PacketExtractor& other) = delete;
 	void operator=(const PacketExtractor& other) = delete;
