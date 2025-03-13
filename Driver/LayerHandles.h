c3fe137b5c184f2ce789c0887ad937b810a97f7d
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2025-03-13 03:18:53 +0200

    beautified code & output

--- Driver/LayerHandles.h
+++ Driver/LayerHandles.h
@@ -2,33 +2,18 @@
 
 // Check if compiling for kernel mode
 #ifdef _KERNEL_MODE
-#include <ntdef.h> // For kernel-mode drivers
 
-#define BASE_MUTEX_DIRECTORY L"\\BaseNamedObjects\\"
 #define BASE_FILE_DIRECTORY L"\\??\\C:\\IDPS_shared_files\\"
 
-typedef struct _KERNEL_OBJECTS
-{
-	PVOID ethernet, internet, transport, application;
-} KERNEL_OBJECTS, *PKERNEL_OBJECTS;
-
 #else // !_KERNEL_MODE
+
+#define BASE_FILE_DIRECTORY L"C:\\IDPS_shared_files\\"
 
 #include <windows.h> // For user-mode applications
 #include <winioctl.h>
 
-#define BASE_MUTEX_DIRECTORY L"Global\\"
-#define BASE_FILE_DIRECTORY L"C:\\IDPS_shared_files\\"
-
 #endif // !_KERNEL_MODE
 
-typedef struct _IOCTL_HANDLES
-{
-	unsigned long pid;
-	HANDLE mutex;
-} IOCTL_HANDLES, *PIOCTL_HANDLES;
-
-#define PACKET_MUTEX_PATH (BASE_MUTEX_DIRECTORY L"packetMutex")
 #define PACKET_FILE_PATH (BASE_FILE_DIRECTORY L"packetFlow.bin")
 
 #define IOCTL_SEND_HANDLES CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
