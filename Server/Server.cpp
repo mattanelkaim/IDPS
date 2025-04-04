6f1d2840eabba49ae3d4a48ce48315611be3c6d9
Gil_Yelkin <gil.yelkin@gmail.com>
2025-04-04 21:02:57 +0300

    fixed blinking and cls command

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -10,8 +10,9 @@ namespace ANSI {
     constexpr std::string_view BLUE = "\033[34;1m"; // Blue and bold
     constexpr std::string_view NORMAL = "\033[0m"; // Resets back to default
     constexpr std::string_view BLINK = "\033[5m"; // Make the following characters blink
-    constexpr std::string_view UP = "\033[A"; // Move up a line
-    constexpr std::string_view DOWN = "\033[B"; // Move down a line
+    constexpr std::string_view UP = "\r\033[A"; // Move to the start of the upper line
+    constexpr std::string_view DOWN = "\r\033[B"; // Move to the start of the bottom line
+    constexpr std::string_view CLS = "\033[3J\033[H\033[2J"; // clears the screen and output buffer
 }
 
 enum command
@@ -33,25 +34,26 @@ static constexpr command hashCommands(const std::string_view cmd) noexcept
 void Server::run()
 {
     std::string userInput;
-    command cmd;
+    command cmd = INVALID_COMMAND;
 
-    std::cout << ANSI::CYAN << "Welcome to our IDPS Server!\ntype \"help\" to view available commands.\n" << ANSI::NORMAL;
+    std::cout << ANSI::CYAN << "Welcome to our IDPS Server!\nType \"help\" to view available commands.\n" << ANSI::NORMAL;
     do
     {
         std::cout << ANSI::BLINK << "> " << ANSI::NORMAL;
         std::cin >> userInput;
-        std::cout << ANSI::UP << "\r> " << userInput << ANSI::DOWN << '\r' << std::flush;
+        std::cin.ignore(__INT_MAX__, '\n'); // discarding everything inputted after the first word
+        std::cout << ANSI::UP << "> " << ANSI::DOWN; // remove blinking from previous line
         cmd = hashCommands(userInput);
         switch (cmd)
         {
         case CLS:
-            system("clear");
+            std::cout << ANSI::CLS; // Do NOT use system("clear") here!
             break;
         case INVALID_COMMAND:
             std::cerr << ANSI::RED << "Invalid command\n" << ANSI::NORMAL;
             break;
         case HELP:
-            std::cout << ANSI::BLUE << "Helping...\n" << ANSI::NORMAL;
+            std::cout << ANSI::BLUE << "Available commands:\n\tcls/clear - clear the screen\n\thelp - show available commands\n\texit - terminate the server\n" << ANSI::NORMAL;
             break;
         case EXIT:
             break;
