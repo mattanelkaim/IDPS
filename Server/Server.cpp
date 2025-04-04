a7aa769b099d5e2ba4ec00e0d1427006ad63f963
Gil_Yelkin <gil.yelkin@gmail.com>
2025-04-04 12:07:48 +0300

    created basic server project and connected it to WSL

---
+++ Server/Server.cpp
@@ -0,0 +1,66 @@
+#include "Server.h"
+#include <iostream>
+#include <string>
+#include <string_view>
+
+namespace ANSI {
+    constexpr std::string_view CYAN = "\033[96m"; // Cyan
+    constexpr std::string_view RED = "\033[31;1m"; // Red and bold
+    constexpr std::string_view GREEN = "\033[32;1m"; // Green and bold
+    constexpr std::string_view BLUE = "\033[34;1m"; // Blue and bold
+    constexpr std::string_view NORMAL = "\033[0m"; // Resets back to default
+    constexpr std::string_view BLINK = "\033[5m";
+}
+
+enum command
+{
+    EXIT,
+    CLS,
+    HELP,
+    INVALID_COMMAND
+};
+
+static constexpr command hashCommands(const std::string_view cmd) noexcept
+{
+    if (cmd == "exit") return EXIT;
+    if (cmd == "cls" || cmd == "clear") return CLS;
+    if (cmd == "help") return HELP;
+    return INVALID_COMMAND;
+}
+
+void Server::run()
+{
+    std::string userInput;
+    command cmd;
+
+    std::cout << ANSI::CYAN << "Welcome to our IDPS Server!\ntype \"help\" to view available commands.\n" << ANSI::NORMAL;
+    do
+    {
+        std::cout << ANSI::BLINK << "> " << ANSI::NORMAL;
+        std::cin >> userInput;
+        cmd = hashCommands(userInput);
+        switch (cmd)
+        {
+        case CLS:
+            system("clear");
+            break;
+        case INVALID_COMMAND:
+            std::cerr << ANSI::RED << "Invalid command\n" << ANSI::NORMAL;
+            break;
+        case HELP:
+            std::cout << ANSI::BLUE << "Helping...\n" << ANSI::NORMAL;
+            break;
+        case EXIT:
+            break;
+        }
+    } while (cmd != EXIT);
+
+    std::cout << ANSI::GREEN << "\nExited server successfully\n" << ANSI::NORMAL;
+}
+
+// Singleton
+Server& Server::getInstance() noexcept
+{
+    static Server instance;
+    return instance;
+}
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,18 +1,7 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include "Handlers/DBHandler.h"
+#include "Server.h"
 
-int main() {
-    int fd = open("example.txt", O_CREAT | O_WRONLY, 0644);
-    if (fd == -1) {
-        perror("Error creating file");
-        return 1;
-    }
-
-    write(fd, "Hello, Linux!\n", 14); // Write to the file
-    close(fd); // Close the file
-
-    printf("File created successfully.\n");
+int main() 
+{
+    Server::getInstance().run();
     return 0;
 }
